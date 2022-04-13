80ead4dfa476f5c5562829ad335245bceffe359b
gideonekekeke <gideonekeke64@gmail.com>
2022-04-13 16:28:36 +0100

    done

--- dbConfig/db.js
+++ dbConfig/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-// const url_offline = 'mongodb://localhost/appSubmissionDB'
+const url_offline = 'mongodb+srv://gideon:NTp46J2P7Efieni@cluster0.7rupp.mongodb.net/appSubmissionDB?retryWrites=true&w=majority'
 const DB_ONLINE = process.env.URL
-mongoose.connect(DB_ONLINE).then(()=>{
+mongoose.connect(url_offline).then(()=>{
     console.log('connect to database successfully');
 })
